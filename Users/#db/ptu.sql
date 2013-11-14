a23885e40b4ccc5076ee493cc65bedb9b62843c1
Baljeet Singh <bsinghs.007@gmail.com>
2013-11-14 23:01:46 +0530

    database added to users/#db directory

---
+++ Users/#db/ptu.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2013 at 09:31 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ptu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_configuration`
+--
+
+CREATE TABLE IF NOT EXISTS `uc_configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `value` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `uc_configuration`
+--
+
+INSERT INTO `uc_configuration` (`id`, `name`, `value`) VALUES
+(1, 'website_name', 'PTU_CSE_SYLLABUS_GUIDE'),
+(2, 'website_url', 'http://localhost/PTU_CSE_SYLLABUS_GUIDE/'),
+(3, 'email', 'bsinghs.007@gmail.com'),
+(4, 'activation', 'false'),
+(5, 'resend_activation_threshold', '0'),
+(6, 'language', 'models/languages/en.php'),
+(7, 'template', 'models/site-templates/default.css');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_pages`
+--
+
+CREATE TABLE IF NOT EXISTS `uc_pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `page` varchar(150) NOT NULL,
+  `private` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+
+--
+-- Dumping data for table `uc_pages`
+--
+
+INSERT INTO `uc_pages` (`id`, `page`, `private`) VALUES
+(2, 'activate-account.php', 0),
+(3, 'admin_configuration.php', 1),
+(4, 'admin_page.php', 1),
+(5, 'admin_pages.php', 1),
+(6, 'admin_permission.php', 1),
+(7, 'admin_permissions.php', 1),
+(8, 'admin_user.php', 1),
+(9, 'admin_users.php', 1),
+(10, 'forgot-password.php', 0),
+(11, 'index.php', 0),
+(12, 'left-nav.php', 0),
+(13, 'login.php', 0),
+(14, 'logout.php', 1),
+(15, 'register.php', 0),
+(16, 'resend-activation.php', 0),
+(17, 'user_settings.php', 1),
+(18, 'account - Copy.php', 0),
+(19, 'file_upload.php', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `uc_permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `uc_permissions`
+--
+
+INSERT INTO `uc_permissions` (`id`, `name`) VALUES
+(1, 'New Member'),
+(2, 'Administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_permission_page_matches`
+--
+
+CREATE TABLE IF NOT EXISTS `uc_permission_page_matches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_id` int(11) NOT NULL,
+  `page_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Dumping data for table `uc_permission_page_matches`
+--
+
+INSERT INTO `uc_permission_page_matches` (`id`, `permission_id`, `page_id`) VALUES
+(2, 1, 14),
+(3, 1, 17),
+(5, 2, 3),
+(6, 2, 4),
+(7, 2, 5),
+(8, 2, 6),
+(9, 2, 7),
+(10, 2, 8),
+(11, 2, 9),
+(12, 2, 14),
+(13, 2, 17);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_users`
+--
+
+CREATE TABLE IF NOT EXISTS `uc_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `display_name` varchar(50) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `activation_token` varchar(225) NOT NULL,
+  `last_activation_request` int(11) NOT NULL,
+  `lost_password_request` tinyint(1) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `sign_up_stamp` int(11) NOT NULL,
+  `last_sign_in_stamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `uc_users`
+--
+
+INSERT INTO `uc_users` (`id`, `user_name`, `display_name`, `password`, `email`, `activation_token`, `last_activation_request`, `lost_password_request`, `active`, `title`, `sign_up_stamp`, `last_sign_in_stamp`) VALUES
+(1, 'sandhus', 'sandhu', '4058321d29d615c8dfe00bcae65a9fdc1cb6aa509dd8ae46f3bd22560914c1105', 'bsinghs.007@gmail.com', '146309436481440b0d3f3188a0d59d13', 1384437552, 0, 1, 'New Member', 1384437552, 1384449578);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uc_user_permission_matches`
+--
+
+CREATE TABLE IF NOT EXISTS `uc_user_permission_matches` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `uc_user_permission_matches`
+--
+
+INSERT INTO `uc_user_permission_matches` (`id`, `user_id`, `permission_id`) VALUES
+(1, 1, 2),
+(2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upload`
+--
+
+CREATE TABLE IF NOT EXISTS `upload` (
+  `username` varchar(50) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `file` varchar(30) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `upload`
+--
+
+INSERT INTO `upload` (`username`, `name`, `email`, `phone`, `file`, `description`) VALUES
+('sandhus', 'Baljeet Singh', 'bsinghs.007@gmail.com', '8591854049', 'cse-03.pdf', 'cse-03.pdf, CSE 2003 syllabus'),
+('sandhus', 'Baljeet Singh', 'bsinghs.007@gmail.com', '8591854049', 'cse-07.pdf', 'cse-07.pdf, CSE 2007 syllabus');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
